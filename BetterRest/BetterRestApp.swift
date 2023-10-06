1bd4f7f460b554f683dde4de0a01b43ec3fad33e
Güray Gül <guraygul59@gmail.com>
2023-10-06 16:11:46 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Güray Gül on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
